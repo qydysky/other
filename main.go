098902b30020d28bea990baea9a4c820dbde652c
qydysky <qydysky@foxmail.com>
2021-03-14 14:36:43 +0800

    update

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
-package main
-
-import (
-	"github.com/qydysky/other/aria2"
-	"github.com/qydysky/other/proxylist"
-	"github.com/qydysky/other/yxw"
-	"github.com/qydysky/other/wt"
-	_ "github.com/qydysky/other/run"
-)
-
-func main(){
-	aria2.Aria2()
-	proxylist.Proxylist()
-	yxw.Yxw()
-	wt.WT()
-}
+package part
